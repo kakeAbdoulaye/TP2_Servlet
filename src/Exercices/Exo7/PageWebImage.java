957c8a6314b7929435f73cf085eb983e293390a6
kakeAbdoulaye <kakeabdoulaye759@gmail.com>
2018-02-20 14:23:42 +0100

    Exo 7

---
+++ src/Exercices/Exo7/PageWebImage.java
@@ -0,0 +1,12 @@
+package Exercices.Exo7;
+
+import javax.servlet.http.HttpServlet;
+
+public class PageWebImage extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9106517198161017349L;
+
+}
